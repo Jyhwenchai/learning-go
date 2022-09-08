86b5f9da30f53480888584e102fb8cf8cda5e7a9
czwpro <cai5301@qq.com>
2022-09-08 20:14:09 +0800

    add samples

---
+++ 14-write-go-code/01-first-program/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"example/user/hello/morestrings"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// func main() {
+// 	fmt.Println("Hello, world.")
+// }
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
