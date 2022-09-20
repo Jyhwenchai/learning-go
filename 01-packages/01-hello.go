f782f355402f3653c126f30fcaa0cd5cafad4d17
czwpro <cai5301@qq.com>
2022-09-20 21:07:15 +0800

    add new sample

--- 01-packages/01-hello.go
+++ 01-packages/01-hello.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+)
 
 // 每一个 go 程序都是从 main 包中的 main 函数开始运行
 func main() {
 	fmt.Printf("Hello, World")
+	var p *[]int = new([]int)      // allocates slice structure; *p == nil; rarely useful
+	var v []int = make([]int, 100) // the slice v now refers to a new array of 100 ints
+
+	fmt.Println(p, v)
+
 }
